603b06b3d939f37e9527c7edf107c7c38036bd3b
Ankish8 <your-email@example.com>
2025-10-26 15:07:05 +0530

    Move Facebook Pixel initialization to useEffect to avoid script injection errors

--- src/components/analytics/FacebookPixel.tsx
+++ src/components/analytics/FacebookPixel.tsx
@@ -37,6 +37,40 @@ export function FacebookPixel({ pixelId }: FacebookPixelProps) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
+  // Initialize pixel on mount
+  useEffect(() => {
+    // Initialize Facebook Pixel
+    if (typeof window !== 'undefined') {
+      // Create fbq stub function
+      (function(f: any, b: any, e: any, v: any, n: any, t: any, s: any) {
+        if (f.fbq) return;
+        n = f.fbq = function() {
+          n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments);
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = '2.0';
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+      })(
+        window,
+        document,
+        'script',
+        'https://connect.facebook.net/en_US/fbevents.js'
+      );
+
+      window.fbq('init', pixelId);
+      window.fbq('track', 'PageView');
+    }
+  }, [pixelId]);
+
   // Track page views on route changes
   useEffect(() => {
     if (typeof window !== 'undefined' && window.fbq) {
@@ -50,26 +84,6 @@ export function FacebookPixel({ pixelId }: FacebookPixelProps) {
 
   return (
     <>
-      {/* Facebook Pixel Base Code */}
-      <Script
-        id="facebook-pixel-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${pixelId}');
-            fbq('track', 'PageView');
-          `,
-        }}
-      />
-
       {/* Facebook Pixel NoScript Fallback */}
       <noscript>
         <img
